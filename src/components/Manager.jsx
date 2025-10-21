d1800f0a6496ecffa405ffd668244eb1fa9d640a
NibiR <npnibirpondit152813@gmail.com>
2025-10-22 00:50:56 +0600

    your table

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -46,37 +46,96 @@ const Manager = () => {
 
       <div className="  mycontainer">
         <h1 className="text-4xl text font-bold text-center">
-         
           <span className="text-green-500">&lt;</span>
           <span>Pass</span>
-          <span className="text-green-500">OP/&gt;</span>
+          <span className="text-green-500">ST/&gt;</span>
         </h1>
-        <p className="text-green-900 text-center text-lg">Your own Password Manager</p>
+        <p className="text-green-900 text-center text-lg">
+          Your own Password Manager
+        </p>
 
         <div className=" flex flex-col p-4 text-black gap-8 items-center">
-          <input value={form.site} onChange={handleChage} placeholder="Enter Website URL" className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black" type="text" name="site" id="" />
+          <input
+            value={form.site}
+            onChange={handleChage}
+            placeholder="Enter Website URL"
+            className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
+            type="text"
+            name="site"
+            id=""
+          />
           <div className="flex w-full justify-between gap-8">
-           <input value={form.username} onChange={handleChage} placeholder="Enter Username" className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black" type="text" name="username" id="" />
-           <div className="relative">
+            <input
+              value={form.username}
+              onChange={handleChage}
+              placeholder="Enter Username"
+              className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
+              type="text"
+              name="username"
+              id=""
+            />
+            <div className="relative">
+              <input
+                value={form.password}
+                onChange={handleChage}
+                placeholder="Enter Password"
+                className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
+                type="text"
+                name="password"
+                id=""
+              />
 
-          
-            <input value={form.password} onChange={handleChage} placeholder="Enter Password" className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black" type="text" name="password" id="" />
-            
-            <span className="absolute right-[3px] top-[4px] cursor-pointer" onClick={showPassword}><img ref={ref} className='p-1' width={26}src="icons/eye.png" alt="eye" /></span>
-          
-
-
-           </div>
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img ref={ref}className="p-1"width={26}src="icons/eye.png"alt="eye"/>
+              </span>
+            </div>
           </div>
-          
-          <button onClick={savePassword} className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900">
+
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/efxgwrkc.json"
-              trigger="hover">
-              
-              </lord-icon>
-            
-            Add Password</button>
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
+        </div>
+        <div className="passwords">
+          <h2 className="font-bold text-2xl py-4">Your Password</h2>
+          {passwordArray.length === 0 && <div>No password to show</div>}
+          {passwordArray.length != 0 && (
+            <table className="table-auto w-full rounded-md overflow-hidden">
+              <thead className="bg-green-800 text-white">
+                <tr>
+                  <th className="py-2">Site</th>
+                  <th className="py-2">Username</th>
+                  <th className="py-2">Password</th>
+                </tr>
+              </thead>
+              <tbody className="bg-green-100">
+                {passwordArray.map((item,index) => {
+                  return <tr key={index}>
+
+                  
+                    <td className="p-2 border border-white text-center w-32">
+                      <a href={item.site} target="_blank">{item.site}</a>
+                    </td>
+                    <td className="p-2 border border-white text-center w-32">
+                      {item.username}
+                    </td>
+                    <td className="p-2 border border-white text-center w-32">
+                      {item.password}
+                    </td>
+                  </tr>;
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
