ed56a126d317c0c716502eaebcd20c4137f44759
NibiR <npnibirpondit152813@gmail.com>
2025-10-27 02:22:25 +0600

    edit&delete

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,7 +1,7 @@
-import React, { use } from "react";
 import { useRef, useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { v4 as uuidv4 } from "uuid";
 
 const Manager = () => {
   const ref = useRef();
@@ -21,11 +21,11 @@ const Manager = () => {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
-      closeOnClick: false,
+      closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "light",
+      theme: "dark",
     });
     navigator.clipboard.writeText(text);
   };
@@ -43,9 +43,59 @@ const Manager = () => {
   };
 
   const savePassword = () => {
-    setPasswordArray([...passwordArray, form]);
-    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
-    console.log([...passwordArray, form]);
+    if (
+      form.site.length > 3 &&
+      form.username.length > 3 &&
+      form.password.length > 3
+    ) {
+      setPasswordArray([...passwordArray, { ...form, id: uuidv4() }]);
+      localStorage.setItem(
+        "passwords",
+        JSON.stringify([...passwordArray, { ...form, id: uuidv4() }])
+      );
+      console.log([...passwordArray, form]);
+      setForm({ site: "", username: "", password: "" });
+
+      toast("Password saved Successfully!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      toast("Error: Password not saved!");
+    }
+  };
+
+  const deletePassword = (id) => {
+    console.log("Deleting password with id ", id);
+    let c = confirm("Are you sure you want to delete this password?");
+    if (c) {
+      setPasswordArray(passwordArray.filter((item) => item.id !== id));
+      localStorage.setItem(
+        "passwords",
+        JSON.stringify(passwordArray.filter((item) => item.id !== id))
+      );
+      toast("Password Deleted!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+  const editPassword = (id) => {
+    console.log("Editing password with id ", id);
+    setForm(passwordArray.filter((i) => i.id === id)[0]);
+    setPasswordArray(passwordArray.filter((item) => item.id !== id));
   };
 
   const handleChage = (e) => {
@@ -58,21 +108,18 @@ const Manager = () => {
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
+        closeOnClick={true}
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light"
-        transition="Bounce"
+        theme="dark"
       />
 
-      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+      <div className="fixed inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
       </div>
 
-      <div className="  mycontainer">
+      <div className="mycontainer min-h-[calc(100vh-112px)]">
         <h1 className="text-4xl text font-bold text-center">
           <span className="text-green-500">&lt;</span>
           <span>Pass</span>
@@ -90,9 +137,9 @@ const Manager = () => {
             className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
             type="text"
             name="site"
-            id=""
+            id="site"
           />
-          <div className="flex w-full justify-between gap-8">
+          <div className="flex flex-col md:flex-row w-full justify-between gap-8">
             <input
               value={form.username}
               onChange={handleChage}
@@ -100,7 +147,7 @@ const Manager = () => {
               className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
               type="text"
               name="username"
-              id=""
+              id="username"
             />
             <div className="relative">
               <input
@@ -111,7 +158,7 @@ const Manager = () => {
                 className="rounded-full border border-green-500 w-full p-4 py-1 bg-white text-black"
                 type="password"
                 name="password"
-                id=""
+                id="password"
               />
 
               <span
@@ -140,11 +187,12 @@ const Manager = () => {
             Save
           </button>
         </div>
+
         <div className="passwords">
           <h2 className="font-bold text-2xl py-4">Your Password</h2>
           {passwordArray.length === 0 && <div>No password to show</div>}
           {passwordArray.length != 0 && (
-            <table className="table-auto w-full rounded-md overflow-hidden">
+            <table className="table-auto w-full rounded-md overflow-hidden mb-10">
               <thead className="bg-green-800 text-white">
                 <tr>
                   <th className="py-2">Site</th>
@@ -226,14 +274,24 @@ const Manager = () => {
                         </div>
                       </td>
                       <td className=" justify-center p-2 border border-white text-center">
-                        <span className="cursor-pointer mx-1">
+                        <span
+                          className="cursor-pointer mx-1"
+                          onClick={() => {
+                            editPassword(item.id);
+                          }}
+                        >
                           <lord-icon
                             src="https://cdn.lordicon.com/gwlusjdu.json"
                             trigger="hover"
                             style={{ width: "25px", height: "25px" }}
                           ></lord-icon>
                         </span>
-                        <span className="cursor-pointer mx-1">
+                        <span
+                          className="cursor-pointer mx-1"
+                          onClick={() => {
+                            deletePassword(item.id);
+                          }}
+                        >
                           <lord-icon
                             src="https://cdn.lordicon.com/skkahier.json"
                             trigger="hover"
